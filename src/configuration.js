9c896a4582ced73e0f27f40de51bbf946da578d1
markie01 <markie01@dixonscarphone.com>
2020-10-16 15:13:03 +0200

    refactor

--- src/configuration.js
+++ src/configuration.js
@@ -7,9 +7,7 @@ export const TYPE_TV_POPULAR = '/tv/popular'
 export const TYPE_MOVIE_POPULAR = '/movie/popular'
 export const POPULAR_MOVIES = `${BASE_URL}${TYPE_MOVIE_POPULAR}${API_KEY}`
 export const POPULAR_TV = `${BASE_URL}${TYPE_TV_POPULAR}${API_KEY}`
-
+export const SEARCH_URL = `${BASE_URL}/search/multi/${API_KEY}`
 export const detailMovie = (movieId, type) =>
 	`${BASE_URL}/${type}/${movieId}${API_KEY}&language=en-US`
-export const SEARCH_URL = `${BASE_URL}/search/multi/${API_KEY}`
-// export const playPageUrl = (movieId) => `/play/${movieId}`
 export const detailMoviePage = (movieId) => `/detail/${movieId}`
